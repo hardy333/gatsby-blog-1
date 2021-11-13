e1d93db4982a0ab5030340ecc9dcc6090ad447c4
gela samsonidze <samosnidze7@yahoo.com>
2021-11-13 11:19:54 +0400

    update to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,17 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout"
 
 const Contact = () => {
+  const [submits, setSubmits] = useState([])
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSubmits([
+      ...submits,
+      {
+        name: e.target.name.value,
+        age: e.target.age.value,
+        id: Math.random(),
+      },
+    ])
+  }
+
   return (
     <Layout>
       <h1>This is Contact page</h1>
-      <form action="https://formspree.io/f/xleaqjln" method="POST">
+      <form action="" method="POST" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
         <label htmlFor="age">Age</label>
         <input type="text" name="age" id="age" />
         <button type="submit">Submit</button>
       </form>
+      <ul>
+        {submits.map((submit, index) => (
+          <li key={submit.id}>
+            {index}
+            <ul>
+              <li>name - {submit.name}</li>
+              <li>age - {submit.age}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
