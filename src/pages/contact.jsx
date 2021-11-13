01665ea70b5ad373a0984d6ae89c57f96322c450
gela samsonidze <samosnidze7@yahoo.com>
2021-11-13 12:37:01 +0400

    s

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -18,13 +18,42 @@ const Contact = () => {
   return (
     <Layout>
       <h1>This is Contact page</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      {/* <form name="contact" method="POST" data-netlify="true">
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
         <label htmlFor="age">Age</label>
         <input type="text" name="age" id="age" />
-        {/* <div data-netlify-recaptcha="true"></div> */}
+        <div data-netlify-recaptcha="true"></div>
         <button type="submit">Submit</button>
+      </form> */}
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
       <ul>
         {submits.map((submit, index) => (
